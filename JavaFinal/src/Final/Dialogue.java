3552ce4169ea7a73236f21e1f0004a3524dfab00
Benjamen Brooks <bb118314@students.davenport.k12.ia.us>
2018-10-09 09:45:42 -0500

    Started on the choices

--- JavaFinal/src/Final/Dialogue.java
+++ JavaFinal/src/Final/Dialogue.java
@@ -4,6 +4,21 @@ public class Dialogue {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] choices = new String[4];
+		String[] choices2 = new String[2];
+		
+		choices[0] = "You get the door open and come across a hallway with two paths. Do you go left or right?";
+		choices[1] = "You go left and find yourself in a kitchen. Your hunger and thirst get to the best of you and you choose between \nthe delicious cake or the tasty milk. ";
+		choices[2] = "You go right and find yourself in a dining room.";
+		choices[3] = "porch";
+		choices[4] = "yard";
+		
+		choices2[0] = "skeleton";
+		choices2[1] = "elevator";
+		choices2[2] = "basement";
+		
+		 System.out.println(choices[0]);
+		 System.out.println(choices[1]);
 
 	}
 
--- JavaFinal/src/Final/Health.java
+++ JavaFinal/src/Final/Health.java
@@ -4,6 +4,7 @@ public class Health {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int health = 100;
 
 	}
 

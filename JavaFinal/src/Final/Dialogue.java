2d4b0069c84ffb62eb63f736abacdad9ea28f4f9
Benjamen Brooks <bb118314@students.davenport.k12.ia.us>
2018-10-10 09:49:01 -0500

    nice

--- JavaFinal/src/Final/Dialogue.java
+++ JavaFinal/src/Final/Dialogue.java
@@ -1,25 +1,75 @@
 package Final;
 
+import java.util.Scanner;
+
 public class Dialogue {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] choices = new String[4];
-		String[] choices2 = new String[2];
+		int choice[]=new int[5];
+		int room = 0;
+		int annoying = 0;
+		String answer;
 		
-		choices[0] = "You get the door open and come across a hallway with two paths. Do you go left or right?";
-		choices[1] = "You go left and find yourself in a kitchen. Your hunger and thirst get to the best of you and you choose between \nthe delicious cake or the tasty milk. ";
-		choices[2] = "You go right and find yourself in a dining room.";
-		choices[3] = "You leave the dining room and end up on the porch.";
-		choices[4] = "You wander into the yard and see a fence too high to jump. There is a doghouse at the other end of the yard.";
+		// This array detirmines which room you are in
 		
-		choices2[0] = "skeleton";
-		choices2[1] = "elevator";
-		choices2[2] = "basement";
+		// Hallway
+		choice[0] = 1;
+		// Dining Room
+		choice[1] = 2;
+		// Porch
+		choice[2] = 3;
+		// Yard
+		choice[3] = 4;
+		// Kitchen
+		choice[4] = 5;  
+ 
+		Scanner input = new Scanner(System.in);
+		System.out.println("You awaken in a room with only a bed. The only way out seems to be the rusted door.");
+		while(room == 0) {
+			System.out.print(">> ");
+			answer = input.next();
+			
+			if (answer.equalsIgnoreCase("check bed")) {
+				System.out.println("You peer under the bed and find a secret hatch");
+			}
+			
+			else if (answer.equalsIgnoreCase("look at bed")) {
+				System.out.println("You peer under the bed and find a secret hatch");
+			}
+			
+			else if (answer.equalsIgnoreCase("look under bed")) {
+				System.out.println("You peer under the bed and find a secret hatch");
+			}
+			
+			else if (answer.equalsIgnoreCase("look out window")) {
+				System.out.println("You gaze out the window but your sight is blinded by darkness");
+			}
+			
+			else if (answer.equalsIgnoreCase("window")) {
+				System.out.println("You gaze out the window but your sight is blinded by darkness");
+			}
+			
+			else if (answer.equalsIgnoreCase("door")) {
+				room = room + 1;
+				hallway(choice, room, answer);
+			}
+			
+			else if (answer.equalsIgnoreCase("open door")) {
+				System.out.println("It doesn't buldge");
+			}
+			
+			else {
+				System.out.println("I don't understand");
+			}
+		}
 		
-		 System.out.println(choices[0]);
-		 System.out.println(choices[1]);
-
 	}
-
+	
+	public static void hallway(int choice[], int room, String answer) {
+		System.out.println("You're able to get the door open and are faced with a hallway leading left and right. Where do you go?");
+		while(room == 1) {
+			System.out.print(">> ");
+			answer = input.next();
+		}
+	}
 }

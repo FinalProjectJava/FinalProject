d16df178bf43eae007e5e157d585496e8cc99ce0
Amia Combs <ac116232@students.davenport.k12.ia.us>
2018-10-16 09:28:26 -0500

    update my working copy

---
+++ WorkingCopy/src/Final/Inventory.java
@@ -0,0 +1,13 @@
+package Final;
+import javax.swing.*;
+import java.awt.*;
+public class Inventory {
+
+	public static void inventory() {
+		// TODO Auto-generated method stub
+		boolean key = false;
+		boolean wireCutters = false;
+
+	}
+
+}

99f92023c01ad3941d42f6287783836c06e313e4
Benjamen Brooks <bb118314@students.davenport.k12.ia.us>
2018-10-16 08:51:19 -0500

    Timer

--- JavaFinal/src/Final/Health.java
+++ JavaFinal/src/Final/Health.java
@@ -1,5 +1,6 @@
 package Final;
-
+import java.util.Timer;
+import java.util.TimerTask;
 public class Health {
 
 	//ADD TIMER
@@ -7,7 +8,22 @@ public class Health {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int health = 100;
+		int i = 0;
 
+		
+		while(i< 10) {
+		    try {
+		    	i++;
+		        //sending the actual Thread of execution to sleep X milliseconds
+		        Thread.sleep(500);
+		    } catch(InterruptedException ie) {}
+		    System.out.println("Hello world!");
+		}
+		
+		
+		System.out.println("Your health is ");
 	}
+	
+	
 
 }
